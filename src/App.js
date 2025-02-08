d6043170a84362109b6958cfb886d2c5b62981d8
JMNaveeth <naveethkinniya2001@gmail.com>
2025-02-08 17:50:26 +0530

    first upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+const Comp1 = () => {
+  return (
+    <>
+    <h1>Hello world</h1>
+    <comp2 />
+    </>
+  )
+}
+
+const comp2 = () => {
+  return(
+    <h2>hi</h2>
+  )
+}
+
+function App() {
+  return (
+ <div className='first_head'>
+  <h3>React</h3>
+  <div>native</div>
+ </div>
+  );
+}
+
+export default App;
